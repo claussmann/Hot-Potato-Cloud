73ec8fe338d2f80d9eb10e22852059b249e0d453
Christian Laußmann <christian@laussmann.me>
2024-03-12 19:26:31 +0100

    ADD: Accept Data stream

--- hot-potato-cloud/src/main.rs
+++ hot-potato-cloud/src/main.rs
@@ -1,15 +1,32 @@
 use std::net::{TcpListener, TcpStream, SocketAddr, Shutdown};
-use std::io::prelude::*;
+use std::io::{prelude::*, BufReader};
+use std::time::{Duration, SystemTime};
 
 const ADDR: &'static str = "127.0.0.1:27491";
+const MAX_REPLICATION: u8 = 1;
+
+
+struct File {
+    data: String,
+    last_seen: SystemTime,
+    replicas_sent: u8,
+}
+
 
 fn main() {
     let listener = TcpListener::bind(ADDR).unwrap();
     let mut peers: Vec<SocketAddr> = Vec::new();
+    let mut files: Vec<File> = Vec::new();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        accept_new_peer(stream, &mut peers)
+        let mut req_type = [0;1];
+        let _ = stream.peek(&mut req_type).unwrap();
+        match req_type[0] {
+            b'p' => accept_new_peer(stream, &mut peers),
+            b'd' => accept_data(stream, &mut files),
+            _ => stream.shutdown(Shutdown::Both).unwrap(),
+        };
     }
 }
 
@@ -33,5 +50,34 @@ fn accept_new_peer(mut stream: TcpStream, peers: &mut Vec<SocketAddr>){
         peers.push(peer);
     }
 
+    return ()
+}
+
+fn accept_data(mut stream: TcpStream, files: &mut Vec<File>){
+    let mut br = BufReader::new(&mut stream);
+    let mut f = File{
+        data: String::new(),
+        last_seen: SystemTime::now(),
+        replicas_sent: 0,
+    };
+
+    let x = br.read_line(&mut f.data).unwrap();
+    f.data.remove(0); // First char is reserved for protocol.
+
+    if x < 1 { // File empty
+        stream.write(b"ERR\n").unwrap();
+    }
+
+    for known in files.iter_mut() {
+        if known.data == f.data { // File known
+            known.last_seen = SystemTime::now();
+            stream.write(b"KNOWN\n").unwrap();
+            stream.shutdown(Shutdown::Both).unwrap();
+            return ()
+        }
+    }
+    stream.write(b"OK\n").unwrap();
+    files.push(f); // File new
+    stream.shutdown(Shutdown::Both).unwrap();
     return ()
 }
